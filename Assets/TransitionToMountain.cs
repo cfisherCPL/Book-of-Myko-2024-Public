ec3202acc57d23d15d1fd1240615c90a9e073d5b
Charles <>
2024-04-22 21:26:39 -0500

    Mountain Added
    mountain scene added and updated for testing.

---
+++ Assets/TransitionToMountain.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class TransitionToMountain : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // Go to forest scene
+            SceneController.instance.LoadScene("Mountain_Outside");
+        }
+    }   
+}
+
